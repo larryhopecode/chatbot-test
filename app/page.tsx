088f5af67fef88887d88b4b5892b42f0ce456151
Larry Hope <larry@sno.ai>
2024-08-07 17:37:23 -0700

    [init commit] with Sno Chatbot in Nextjs template app

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,40 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const iframe = document.createElement("iframe");
+
+    const iframeStyles = (styleString: string | null) => {
+      const style = document.createElement('style');
+      style.textContent = styleString;
+      document.head.append(style);
+    }
+
+    iframeStyles(`
+      .chat-frame {
+        position: fixed;
+        bottom: 50px;
+        right: 50px;
+        border: none;
+      }
+    `);
+
+    iframe.src = "http://localhost:3000/chatbot";
+    iframe.classList.add('chat-frame');
+    document.body.appendChild(iframe);
+
+    window.addEventListener("message", (e) => {
+      if (e.origin !== "http://localhost:3000") return null;
+      let dimensions = JSON.parse(e.data);
+      iframe.width = dimensions.width;
+      iframe.height = dimensions.height;
+      iframe.contentWindow?.postMessage("66aebfd6574ba8e7c7febf38", "http://localhost:3000/");
+    });
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
